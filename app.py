55d41c2acdc6360b8e794683ebf74fe90b3e4059
ayanshpandit9 <kartikmishra6803@gmail.com>
2025-07-19 23:38:28 +0530

    Update app.py

--- app.py
+++ app.py
@@ -157,5 +157,6 @@ def stop_sending():
     stop_event.set()
     return '✅ Sending stopped.'
 
+import os
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=int(os.getenv("PORT", 5000)))
